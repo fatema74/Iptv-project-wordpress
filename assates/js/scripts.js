7d744e9ff7f65f16c85ef5026f01a0fe104de788
Kanis Fatema <fatema26721@gmail.com>
2025-05-23 00:12:03 +0600

    slider is completely fine

--- assates/js/scripts.js
+++ assates/js/scripts.js
@@ -7,7 +7,33 @@
     
   });
 
+  var swiper = new Swiper('.ip-series-slider.slider-ltr', {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    speed: 3000,
+    loop: true,
+    autoplay: {
+      delay: 0,
+      enabled: true,
+    }
+  });
 
+
+  var swiper = new Swiper('.ip-series-slider.slider-rtl', {
+    // direction: 'rtl',
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    speed: 3000,
+    loop: true,
+    autoplay: {
+      delay: 0,
+      enabled: true,
+    },
+  });
+
+
+
+  
 })(jQuery)
 
 
