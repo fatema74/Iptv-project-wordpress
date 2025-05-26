7ce73a6d91fd2d8d91bb986f645b5b590a363e0b
Kanis Fatema <fatema26721@gmail.com>
2025-05-26 23:13:31 +0600

    brand slider section completely

--- assates/js/scripts.js
+++ assates/js/scripts.js
@@ -1,13 +1,13 @@
 (function($) {
   
-  var swiper = new Swiper('.ip-hero-slider', {
+  var heroSlider = new Swiper('.ip-hero-slider', {
     effect: 'cards',
     grabCursor: true,
     initialSlide: 1,
     
   });
 
-  var swiper = new Swiper('.ip-series-slider.slider-ltr', {
+  var serisSlider = new Swiper('.ip-series-slider.slider-ltr', {
     slidesPerView: 'auto',
     spaceBetween: 24,
     speed: 3000,
@@ -19,10 +19,23 @@
   });
 
 
-  var swiper = new Swiper('.ip-series-slider.slider-rtl', {
+  var serisSliderRtl = new Swiper('.ip-series-slider.slider-rtl', {
     // direction: 'rtl',
     slidesPerView: 'auto',
     spaceBetween: 24,
+    speed: 3000,
+    loop: true,
+    autoplay: {
+      delay: 0,
+      enabled: true,
+    },
+  });
+
+
+  // brand slider
+  var brandSlider = new Swiper('.ip-brand-slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 40,
     speed: 3000,
     loop: true,
     autoplay: {
